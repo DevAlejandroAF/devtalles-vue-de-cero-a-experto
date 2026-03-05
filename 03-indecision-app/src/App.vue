f09beed74e0874c58b32440fe75789fc7cca4455
devalejandroaf <alejandroavi95@hotmail.com>
2026-03-05 18:57:57 -0500

    Section 5 in progress

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,22 +1,7 @@
 <script lang="ts" setup>
-import MyCounter from './components/counter/CounterTemplate.vue';
-import MyCounter2 from './components/MyCounter.vue';
+import IndecisionView from '@/views/IndecisionView.vue';
 </script>
 
 <template>
-  <main class="grid justify-center">
-    <h2 class="text-3xl font-bold mt-3 text-white">My First Vue App</h2>
-    <br />
-    <MyCounter :initialValue="5" />
-    <MyCounter2 :initialValue="4" />
-  </main>
+  <IndecisionView />
 </template>
-
-<style>
-@reference "tailwindcss";
-@layer base {
-  body {
-    @apply bg-slate-800;
-  }
-}
-</style>
---
+++ 03-indecision-app/src/components/chat/ChatMessages.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import ChatBubble from './ChatBubble.vue';
+</script>
+
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <ChatBubble :its-mine="true" :message="'Hola'" />
+      <ChatBubble
+        :its-mine="false"
+        :message="'no'"
+        image="https://yesno.wtf/assets/no/24-159febcfd655625c38c147b65e5be565.gif"
+      />
+    </div>
+  </div>
+</template>
